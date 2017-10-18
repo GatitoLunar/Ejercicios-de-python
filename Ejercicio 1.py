84280d252d205c8e18c42ccbea964b6bf2a50351
GatitoLunar <lunothesoldier@gmail.com>
2017-10-18 20:35:46 -0300

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,7 @@
+
+
+def my_max(value_a,value_b):
+	if value_a > value_b:
+		return value_a
+	else:
+		return value_b
---
+++ Ejercicio 2.py
@@ -0,0 +1,4 @@
+def max_de_tres(value_a, value_b, value_c):
+	aux_a = max(value_a,value_b)
+	aux_b = max(aux_a, value_c)
+	return aux_b
