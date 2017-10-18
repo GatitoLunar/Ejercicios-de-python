a8f37a2309e76e460f116756fbfd939e5c776b83
GatitoLunar <lunothesoldier@gmail.com>
2017-10-18 20:49:09 -0300

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,5 @@
+def my_len(lst):
+	i = 0
+	for item in lst:
+		i+=1
+	return i
---
+++ Ejercicio 4.py
@@ -0,0 +1,5 @@
+def esvocal(caracter):
+	if caracter in 'aeiou' or caracter in 'AEIOU':
+		return True
+	else:
+		return False
