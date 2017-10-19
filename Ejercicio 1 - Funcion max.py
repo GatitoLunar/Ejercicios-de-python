f61dc37220cfff9cec3ad6078b1b9b6428319ad5
GatitoLunar <lunothesoldier@gmail.com>
2017-10-18 22:05:08 -0300

    Update and rename Ejercicio 1 - Funcion max.py to Ejercicio 1 - Funcion my_max.py

--- Ejercicio 1 - Funcion max.py
+++
@@ -1,7 +0,0 @@
-
-
-def my_max(value_a,value_b):
-	if value_a > value_b:
-		return value_a
-	else:
-		return value_b
---
+++ Ejercicio 1 - Funcion my_max.py
@@ -0,0 +1,8 @@
+#Definir una función my_max() que tome como argumento dos números
+#y devuelva el mayor de ellos.
+
+def my_max(value_a,value_b):
+	if value_a > value_b:
+		return value_a
+	else:
+		return value_b
