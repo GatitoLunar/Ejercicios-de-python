37aee2e75a57057bd5ea26c4ec874d98667ae2b6
GatitoLunar <lunothesoldier@gmail.com>
2017-10-18 22:07:41 -0300

    Update and rename Ejercicio 3 - Funcion len.py to Ejercicio 03 - Funcion len.py

---
+++ Ejercicio 03 - Funcion len.py
@@ -0,0 +1,8 @@
+#Definir una función que calcule la 
+#longitud de una lista o una cadena dada. 
+
+def my_len(lst):
+	i = 0
+	for item in lst:
+		i+=1
+	return i
--- Ejercicio 3 - Funcion len.py
+++
@@ -1,5 +0,0 @@
-def my_len(lst):
-	i = 0
-	for item in lst:
-		i+=1
-	return i
