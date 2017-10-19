f87ea4818c562c685ee3ddd2dfa0c0f2ad310e30
GatitoLunar <lunothesoldier@gmail.com>
2017-10-18 22:06:46 -0300

    Update and rename Ejercicio 2- Funcion max de 3.py to Ejercicio 02- Funcion max_de_3.py

--- Ejercicio 02- Funcion max_de_3.py
+++ Ejercicio 02- Funcion max_de_3.py
@@ -1,3 +1,6 @@
+#Definir una función max_de_tres(), que tome tres números
+#como argumentos y devuelva el mayor de ellos.
+
 def max_de_tres(value_a, value_b, value_c):
 	aux_a = max(value_a,value_b)
 	aux_b = max(aux_a, value_c)
